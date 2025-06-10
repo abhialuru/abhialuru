a54a6402f5c1d5c8730c568f9f118853d3b10b16
abhialuru <alurabhi9346@gmail.com>
2025-06-11 00:17:32 +0530

    unneccesary font removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Lavishly_Yours, Open_Sans } from "next/font/google";
-import "./globals.css";
 import ReactLenis from "lenis/react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Lavishly_Yours, Open_Sans } from "next/font/google";
+import "./globals.css";
 
 const lavishly = Lavishly_Yours({
   variable: "--font-lavishly",
